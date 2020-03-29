b07acd74feeb5b6f8922084315e05a5edd8c61d7
solmazlotfi <48198088+solmazlotfi@users.noreply.github.com>
2020-03-29 10:18:15 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const images = ['fox1','fox2','fox3','fox4'];
-const imgElem = document.querySelector('img');
+//const images = ['fox1','fox2','fox3','fox4'];
+//const imgElem = document.querySelector('img');
 
-function randomValueFromArray(array) {
-  let randomNo =  Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
+//function randomValueFromArray(array) {
+//  let randomNo =  Math.floor(Math.random() * array.length);
+//  return array[randomNo];
+//}
 
-setInterval(function() {
-  let randomChoice = randomValueFromArray(images);
-  imgElem.src = 'images/' + randomChoice + '.jpg';
- // imgElem.src = 'http://www.kiccc.com/Content/_Layouts/Spring_Home.fa-IR/imgs/irankish_logo.png';
-}, 2000)
+//setInterval(function() {
+//  let randomChoice = randomValueFromArray(images);
+//  imgElem.src = 'images/' + randomChoice + '.jpg';
+// // imgElem.src = 'http://www.kiccc.com/Content/_Layouts/Spring_Home.fa-IR/imgs/irankish_logo.png';
+//}, 2000)
 
-// Register service worker to control making site work offline
+//// Register service worker to control making site work offline
 
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('/pwa-examples/a2hs/sw.js')
-           .then(function() { console.log('Service Worker Registered'); });
-}
+//if('serviceWorker' in navigator) {
+//  navigator.serviceWorker
+//           .register('/pwa-examples/a2hs/sw.js')
+//           .then(function() { console.log('Service Worker Registered'); });
+//}
 
 // Code to handle install prompt on desktop
 
